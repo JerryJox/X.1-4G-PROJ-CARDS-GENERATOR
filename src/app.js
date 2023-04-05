b9575b8382d570db0c72929290fa865d226160bb
JerryJox <profesionalmail@msn.com>
2023-04-05 19:40:41 +0000

    upload files

--- src/app.js
+++ src/app.js
@@ -7,5 +7,29 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let palos = ["corazon", "pica", "diamante", "trebol"];
+  let numeros = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "10",
+    "rey",
+    "reina",
+    "j",
+    "as"
+  ];
+
+  // function generateExcuse() {
+  let randomPalosIndex = Math.floor(Math.random() * palos.length);
+  let randomNumerosIndex = Math.floor(Math.random() * numeros.length);
+
+  // return (
+  //   palos[randomPalosIndex]
+  console.log(randomPalosIndex);
+  console.log(palos[randomPalosIndex]);
+  // )  }
 };
